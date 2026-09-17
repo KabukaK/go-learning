1648aa96a001ad4698aa299148f1d1d87867c4ce
KabukaK <kskulov3@gmail.com>
2026-09-17 01:42:25 -0700

    Функции, defer, структура Expense и метод Classify

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Printf("Hello world!")
+
+	// var amount float64 = 500
+	// var category string = "Еда"
+	// var datespend string = "2026-09-17"
+	// var paycard bool = true
+	var limit float64 = 1000
+	e := Expense{Amount: 500, Category: "Еда", Date: "2026-09-17", Paycard: true}
+	defer fmt.Println("Прогграмма окончена")
+	// fmt.Println(classify(amount, limit))
+	for i := 1; i <= 7; i++ {
+		fmt.Println(i)
+	}
+	// fmt.Println(amount, category, datespend, paycard)
+	fmt.Println(total(500, 100, 1000))
+	fmt.Println(e.Classify(limit))
+}
+
+func (e Expense) Classify(limit float64) (string, bool) {
+
+	if e.Amount > limit {
+		return "Крупная трата", true
+	} else {
+		return "Мелкая трата", false
+	}
+
+}
+
+func total(amounts ...float64) float64 {
+	var sum float64
+	for _, amount := range amounts {
+		sum += amount
+	}
+	return sum
+}
+
+type Expense struct {
+	Amount   float64
+	Category string
+	Date     string
+	Paycard  bool
 }
